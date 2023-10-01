28aaa80d23ffd41ea3e8481100e68fc44cea6961
Suljov <albin.suljov@hotmail.com>
2023-10-01 20:17:12 +0200

    Add files via upload

---
+++ kali-setup.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+mkdir tools
+cd tools
+
+mkdir web
+mkdir system
+mkdir system/linux
+mkdir system/windows
+mkdir pivoting
+
+wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_amd64 
+wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh
+wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS.bat 
+wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS.exe
+
+mv linpeas_linux_amd64 linpeas.sh system/linux/
+mv winPEAS.bat winPEAS.exe system/windows/
+
+pip3 install kerbrute
+
+git clone https://github.com/AlessandroZ/LaZagne
+cd LaZagne
+wget https://github.com/AlessandroZ/LaZagne/releases/latest/download/LaZagne.exe 
+cd ..
+
+mv LaZagne system/windows/
+
+wget https://github.com/SnaffCon/Snaffler/releases/latest/download/Snaffler.exe 
+wget https://download.sysinternals.com/files/Procdump.zip
+wget https://github.com/itm4n/PrintSpoofer/releases/latest/download/PrintSpoofer32.exe 
+wget https://github.com/itm4n/PrintSpoofer/releases/latest/download/PrintSpoofer64.exe 
+
+mv Snaffler.exe Procdump.zip PrintSpoofer32.exe PrintSpoofer64.exe system/windows/
+
+mkdir potato
+cd potato
+git clone https://github.com/breenmachine/RottenPotatoNG
+git clone https://github.com/antonioCoco/RoguePotato
+git clone https://github.com/ohpe/juicy-potato
+git clone https://github.com/CCob/SweetPotato
+wget https://github.com/ohpe/juicy-potato/releases/latest/download/JuicyPotato.exe 
+cd ..
+
+mv potato system/windows/
+
+wget https://download.sysinternals.com/files/AccessChk.zip
+
+mv AccessChk.zip system/windows/
+
+mkdir ligolo-ng
+cd ligolo-ng
+mkdir proxy
+mkdir agen
+git clone https://github.com/nicocha30/ligolo-ng
+cd agen
+wget https://github.com/nicocha30/ligolo-ng/releases/latest/download/ligolo-ng_agent_0.4.4_linux_amd64.tar.gz 
+wget https://github.com/nicocha30/ligolo-ng/releases/latest/download/ligolo-ng_agent_0.4.4_windows_amd64.zip 
+cd ..
+cd proxy
+wget https://github.com/nicocha30/ligolo-ng/releases/latest/download/ligolo-ng_proxy_0.4.4_linux_amd64.tar.gz 
+wget https://github.com/nicocha30/ligolo-ng/releases/latest/download/ligolo-ng_proxy_0.4.4_windows_amd64.zip 
+cd ..
+cd ..
+
+mv ligolo-ng pivoting/
+
+wget https://github.com/diego-treitos/linux-smart-enumeration/releases/latest/download/lse.sh 
+
+mv lse.sh system/linux/
+
+wget https://github.com/DominicBreuker/pspy/releases/latest/download/pspy32 
+wget https://github.com/DominicBreuker/pspy/releases/latest/download/pspy64
+
+mv pspy32 pspy64 system/linux/
+
+sudo apt install --only-upgrade firefox-esr
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install sublime-text
+
+# List of tools to install using apt
+tools_to_install=(
+    "terminator"
+    "gobuster"
+    "feroxbuster"
+    "dirsearch"
+    "seclists"
+    "remmina"
+    "openjdk-11-jdk"
+    "neo4j"
+    "bloodhound"
+    "gedit"
+    "flameshot"
+    "code-oss"
+
+)
+
+# Install the tools using apt
+echo "Installing tools using apt..."
+
+# updates the system
+sudo apt install apt-transport-https
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt full-upgrade -y 
+sudo apt dist-upgrade -y
+
+# extra command to make sure autorecon works
+sudo apt install seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf
+
+sudo apt install python3-venv
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+
+python3 -m pipx install impacket
+
+pipx install git+https://github.com/Tib3rius/AutoRecon.git
+
+# installs all the tools with apt
+sudo apt install -y "${tools_to_install[@]}"
+sudo apt update -y
+sudo apt upgrade -y
+
+# GitHub repository URLs
+repo_urls=(
+    "https://github.com/r3motecontrol/Ghostpack-CompiledBinaries"
+    "https://github.com/Kevin-Robertson/Inveigh"
+    "https://github.com/AonCyberLabs/Windows-Exploit-Suggester"
+    "https://github.com/ropnop/windapsearch"
+    "https://github.com/GhostPack/Rubeus"
+    "https://github.com/itm4n/PrivescCheck"
+    "https://github.com/ParrotSec/mimikatz"
+    "https://github.com/int0x33/nc.exe"
+    "https://github.com/topotam/PetitPotam"
+    "https://github.com/rebootuser/LinEnum"
+    "https://github.com/The-Z-Labs/linux-exploit-suggester"
+    "https://github.com/jondonas/linux-exploit-suggester-2"
+    "https://github.com/pentestmonkey/php-reverse-shell"
+    "https://github.com/epinna/tplmap"
+    "https://github.com/enjoiz/XXEinjector"
+    "https://github.com/Ebryx/GitDump"
+    "https://github.com/bugcrowd/HUNT"
+    "https://github.com/WhiteWinterWolf/wwwolf-php-webshell"
+    "https://github.com/GhostPack/SharpUp"
+    "https://github.com/GhostPack/Seatbelt"
+    "https://github.com/fortra/impacket"
+    "https://github.com/dirkjanm/BloodHound.py"
+    "https://github.com/BloodHoundAD/BloodHound"
+    
+)
+
+# Loop through the repository URLs and clone each one into the current directory
+for url in "${repo_urls[@]}"; do
+    repo_name=$(basename "$url" .git)
+    echo "Cloning $repo_name..."
+    git clone "$url"
+done
+
+mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester windapsearch Rubeus PrivescCheck mimikatz nc.exe PetitPotam SharpUp Seatbelt impacket BloodHound.py BloodHound system/windows/
+mv linux-exploit-suggester linux-exploit-suggester-2 php-reverse-shell tplmap XXEinjector GitDump HUNT wwwolf-php-webshell LinEnum system/linux/
+
+
+cd ..
+sudo chown -R $USER:$USER tools
+
+git clone https://github.com/Dewalt-arch/pimpmykali
+cd pimpmykali
+echo "1" | sudo ./pimpmykali.sh
+echo "3" | sudo ./pimpmykali.sh
+echo "5" | sudo ./pimpmykali.sh
+cd .. 
+rm -r pimpmykali
+
+clear
+
+sudo rm -r /opt/linpeas/ 
+sudo rm -r /opt/mitm6/
+sudo rm -r /opt/winpeas/
+
+clear
+
+echo "Tools and repositories cloned successfully."
+echo " "
+echo "Make sure you add the tools and folders in the right folder to make it more organised"
+echo " "
+echo "Manual installs on several tools/depencies are neeed"
+echo "Recommended are:"
+echo "1. Obsidian"
+echo "2. nuclei"
+echo "3. PwnKit"
+echo "4. DirtyCow"
+echo "5. Printnightmare"
+echo "6. chisel"
+echo "7. plink"
+echo "8. Enum4linux-ng"
+echo "9. postman"
+echo " "
+echo "IMPORTANT"
+echo "run 'sudo chown -R <your username>:<your username> tools' for the permissions to be correct"
+echo " "
+echo "All Done! Happy Hacking!"
+
+
+
+
+
+
