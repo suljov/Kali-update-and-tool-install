cd56773aaf49bb686878b27e647d5168dd23555f
Suljov <albin.suljov@hotmail.com>
2024-03-05 21:39:46 +0100

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -66,7 +66,8 @@ wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS.ba
 wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64.exe 
 wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64_ofs.exe 
 wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx86.exe 
-wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany.exe 
+wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany.exe
+wget https://github.com/antonioCoco/RunasCs/releases/latest/download/RunasCs.zip
 wget https://raw.githubusercontent.com/Tib3rius/windowsprivchecker/master/windowsprivchecker.bat
 https://github.com/itm4n/FullPowers/releases/latest/download/FullPowers.exe
 
@@ -75,7 +76,7 @@ pip3 install xcat
 wget https://download.sysinternals.com/files/SysinternalsSuite.zip
 
 mv linpeas_linux_amd64 linpeas.sh system/linux/
-mv winPEAS.bat winPEASx64.exe winPEASx64_ofs.exe SysinternalsSuite.zip  winPEASx86.exe winPEASany.exe FullPowers.exe windowsprivchecker.bat system/windows/
+mv winPEAS.bat winPEASx64.exe winPEASx64_ofs.exe SysinternalsSuite.zip  winPEASx86.exe winPEASany.exe RunasCs.zip FullPowers.exe windowsprivchecker.bat system/windows/
 
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
 sudo dpkg -i rustscan_2.0.1_amd64.deb
