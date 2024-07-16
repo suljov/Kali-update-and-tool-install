5d673fc4b0f3316dbe7fa4d65da1e583b6462bf8
Suljov <albin.suljov@hotmail.com>
2024-07-16 17:18:29 +0200

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -78,6 +78,11 @@ cd c2
 git clone https://github.com/BishopFox/sliver.git
 cd sliver
 make
+sudo cp sliver-server /usr/local/bin/sliver-server
+sudo cp sliver-client /usr/local/bin/sliver
+sudo chmod 755 /usr/local/bin/sliver-server
+sudo chmod 755 /usr/local/bin/sliver
+sliver-server unpack --force
 cd ..
 cd ..
 
