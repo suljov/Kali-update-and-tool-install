fd0b154aff29c6e067d87996951c5ebd17f6d908
Suljov <albin.suljov@hotmail.com>
2024-09-21 10:47:14 +0200

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -224,14 +224,7 @@ python3 -m pipx ensurepath
 apt install python3 python3-pip -y
 sudo apt install python3 python3-pip
 
-pipx install git+https://github.com/Pennyw0rth/NetExec
-git clone https://github.com/Pennyw0rth/NetExec
-cd NetExec
-python3 -m venv .
-source bin/activate
-pip install .
-cd ..
-sudo rm -r NetExec
+
 
 
 python3 -m pipx install impacket
