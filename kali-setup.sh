16aba5cd267e92017ab0dd6e2e93796757b2b067
Suljov <albin.suljov@hotmail.com>
2025-01-18 00:14:15 +0100

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -324,6 +324,7 @@ repo_urls=(
     "https://github.com/yuyudhn/AzSubEnum"
     "https://github.com/dievus/Oh365UserFinder"
     "https://github.com/0xZDH/o365spray cloud/Azure/o365spray"
+    "https://github.com/joswr1ght/basicblobfinder"
 )
 
 # Loop through the repository URLs and clone each one into the current directory
@@ -336,7 +337,7 @@ done
 mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester ntlm_theft windapsearch Rubeus PrivescCheck mimikatz nc.exe PetitPotam SharpUp CVE-2023-2255 Seatbelt bloodyAD krbrelayx impacket PrecompiledBinaries SharpCollection BloodHound.py adPEAS windowsprivchecker BloodHound donut powersploit system/windows/
 mv linux-exploit-suggester linux-exploit-suggester-2 LinEnum linuxprivchecker system/linux/
 mv Pentest-Cheatsheets misc/
-mv TokenTacticsV2 AzSubEnum Oh365UserFinder o365spray cloud/Azure
+mv TokenTacticsV2 AzSubEnum Oh365UserFinder o365spray basicblobfinder cloud/Azure
 
 
 wget https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET2.exe
