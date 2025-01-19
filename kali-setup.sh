9d54f4516d372840c129186bf5f19201da693693
Suljov <albin.suljov@hotmail.com>
2025-01-19 20:19:47 +0100

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -83,6 +83,7 @@ mkdir cloud/AWS
 wget https://raw.githubusercontent.com/rootsecdev/Azure-Red-Team/master/Tokens/exfil_exchange_mail.py -O tools/cloud/Azure/exfil_exchange_mail.py
 wget https://raw.githubusercontent.com/dafthack/MFASweep/master/MFASweep.ps1 -O tools/cloud/Azure/MFASweep.ps1
 wget https://raw.githubusercontent.com/dafthack/MSOLSpray/refs/heads/master/MSOLSpray.ps1 -O cloud/Azure/MSOLSpray.ps1
+wget https://raw.githubusercontent.com/BenTamam/PentestPlayground/refs/heads/main/Azure/Scripts/CheckScopedRolePrivileges.ps1 -O cloud/Azure/CheckScopedRolePrivileges.ps1
 
 git clone https://github.com/ly4k/Certipy.git
 cd Certipy
